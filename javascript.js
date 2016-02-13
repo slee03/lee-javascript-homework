ee1f892e2a8d4408083d575cf089ee8d3daee237
samantha lee <samantha.lee@journalism.cuny.edu>
2016-02-13 13:34:00 -0500

    COMMIT

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var fifty = 50 
+var hundred = 100 
+
+var addition = fifty + hundred
+var multiply = fifty * hundred
+var divide = hundred / fifty  
+var subtract = hundred - fifty
+
+document.getElementById("addition").innerHTML = "When I add " + fifty + " to " + hundred + " I get " + addition; 
+document.getElementById("subtract").innerHTML = "When I subtract " + fifty + " from " + hundred + " I get " + subtract;
+document.getElementById("multiply").innerHTML = "When I multiply " + fifty + " by " + hundred + " I get " + multiply;
+document.getElementById("divide").innerHTML = "When I divide " + hundred + " by " + fifty + " I get " + divide;
+
+
